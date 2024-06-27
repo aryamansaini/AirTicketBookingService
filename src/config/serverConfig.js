45e913d931f6808ef9dab1d22073a6b215f79b2f
Aryaman Saini <aryamansaini10@gmail.com>
2024-06-27 19:01:47 +0530

    completed Create Booking

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,5 +3,6 @@ const dontenv = require('dotenv');
 dontenv.config();
 
 module.export={
-        PORT:process.env.PORT
+        PORT:process.env.PORT,
+        FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH
 }
